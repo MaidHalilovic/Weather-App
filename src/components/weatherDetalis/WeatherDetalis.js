27b5e9fbf8525ae80c59078f97f8c6594ea87d7c
MaidHalilovic <maidhaliloviclimunada567@gmail.com>
2025-03-21 00:08:47 +0100

    Made Second card

--- src/components/weatherDetalis/WeatherDetalis.js
+++ src/components/weatherDetalis/WeatherDetalis.js
@@ -30,18 +30,30 @@ const WeatherDetalis = () => {
       <h1>WeatherDetalis</h1>
       <div className='mainDiv'>
         <div className='temp'>
-          <div className='text'>
+          <div className='card'>
             <h3>Feels like</h3>
             <h1>{weatherDetalis.main.feels_like}C°</h1>
-
-            <h3>Feels like</h3>
-
+          </div>
+          <div className='card'>
+            <h3>Wind Speed</h3>
             <h1>{weatherDetalis.wind.speed}km/h</h1>
-            <h3>Feels like</h3>
-
+          </div>
+          <div className='card'>
+            <h3>Humidity</h3>
             <h1>{weatherDetalis.main.humidity}%</h1>
           </div>
-          <div className='weather-parameters'></div>
+          <div className='card'>
+            <h3>Pressure</h3>
+            <h1>{weatherDetalis.main.pressure}hPa</h1>
+          </div>
+          <div className='card'>
+            <h3>Clouds</h3>
+            <h1>{weatherDetalis.clouds.all}</h1>
+          </div>
+          <div className='card'>
+            <h3>Visibility</h3>
+            <h1>{weatherDetalis.visibility / 1000} km</h1>
+          </div>
         </div>
       </div>
     </div>
